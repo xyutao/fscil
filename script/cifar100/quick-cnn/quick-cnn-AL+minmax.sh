de5b3376a2a6891cb72dba46ccf95d73b33f2750
Xiaoyu Tao <42927482+xyutao@users.noreply.github.com>
2020-08-27 15:47:39 +0800

    Add files via upload

---
+++ script/cifar100/quick-cnn/quick-cnn-AL+minmax.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+python ./tools/train_cifar100.py \
+    --gpus 8 \
+    --wd 0.0005 \
+    --batch-size 128 \
+    --model quick_cnn \
+    --resume-from './params/CIFAR100/quick_cnn/baseline/baseline-62.52-57.78.params' \
+    --save-name _plc_fw_new_mimx_w0.3 \
+    --sess-num 9 \
+    --lrs 0.1 \
+    --lr-decay 0.1 \
+    --base-decay-epoch 40 \
+    --inc-decay-epoch 60 \
+    --epoch 70 \
+    --use-AL \
+    --fw \
+    --use-ng-min \
+    --min-weight 0.001 \
+    --ng-min-lr 0.1 \
+    --use-ng-max \
+    --max-weight 0.0001 \
+    --c-way 5 \
+    --k-shot 5 \
+    --base-acc 57.78 \
+    --dataset 'NC_CIFAR100' \
+    --select-best 'select_best2'
