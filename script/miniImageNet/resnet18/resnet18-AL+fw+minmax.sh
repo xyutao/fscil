9763a233ad9c4afcf10516d7c94b0781d4cdeeef
Xiaoyu Tao <42927482+xyutao@users.noreply.github.com>
2020-08-27 16:03:25 +0800

    Add files via upload

---
+++ script/miniImageNet/resnet18/resnet18-AL+fw+minmax.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+python ./tools/train_mini_imagenet.py \
+    --gpus 5 \
+    --wd 0.0005 \
+    --batch-size 128 \
+    --model resnet18 \
+    --resume-from './params/MINI_IMAGENET/resnet18/baseline/baseline_resnet18_mini_61.31.params' \
+    --save-name '_fix+fw+min' \
+    --sess-num 9 \
+    --lrs 0.1 \
+    --lr-decay 0.1 \
+    --base-decay-epoch 40,60 \
+    --inc-decay-epoch 90 \
+    --epoch 100 \
+    --fix-conv \
+    --fix-epoch 100 \
+    --wo-bn \
+    --fw \
+    --use-AL \
+    --use-ng \
+    --ng-var \
+    --AL-weight 350 \
+    --use-ng-min \
+    --min-weight 0.3 \
+    --ng-min-lr 0.1 \
+    --use-ng-max \
+    --max-weight 0.00001 \
+    --c-way 5 \
+    --k-shot 5 \
+    --base-acc 61.31 \
+    --dataset 'NC_MiniImageNet' \
+    --select-best 'select_best2'
