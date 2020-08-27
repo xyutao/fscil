3a1f12d72e638a3c3f53d644e88bb40a00b2ebae
Xiaoyu Tao <42927482+xyutao@users.noreply.github.com>
2020-08-27 12:27:14 +0800

    Create resnet18-AL+fw.sh

---
+++ script/cifar100/resnet18/resnet18-AL+fw.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+python ./tools/train_cifar100.py \
+    --gpus 7 \
+    --wd 0.0005 \
+    --batch-size 128 \
+    --model resnet18 \
+    --resume-from './params/CIFAR100/resnet18/baseline/baseline_resnet18_cifar_64.10.params' \
+    --save-name '_plc_350+fw '\
+    --sess-num 9 \
+    --lrs 0.1 \
+    --lr-decay 0.1 \
+    --base-decay-epoch 40 \
+    --inc-decay-epoch 60 \
+    --epoch 70 \
+    --use-AL \
+    --AL-weight 350 \
+    --wo-bn \
+    --use-ng \
+    --ng_var \
+    --fw \
+    --fix-conv \
+    --fix-epoch 100 \
+    --c-way 5 \
+    --k-shot 5 \
+    --base-acc 64.10 \
+    --dataset 'NC_CIFAR100' \
+    --select-best 'select_best2'
